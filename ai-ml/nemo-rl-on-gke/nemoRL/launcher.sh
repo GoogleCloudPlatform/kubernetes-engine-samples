ceda7e631d627f74a66fc9eb63f4de6d6b4169b1
Prem Pradeep Motgi <pmotgi@google.com>
2026-03-24 15:37:51 -0500

    updated placeholder

--- ai-ml/nemo-rl-on-gke/nemoRL/launcher.sh
+++ ai-ml/nemo-rl-on-gke/nemoRL/launcher.sh
@@ -16,5 +16,5 @@
 #!/bin/bash
 REPLICA_COUNT=4
 
-helm install ray-cluster "/Users/pmotgi/exploration/Next 2026/nemo-rl-on-gke/nemoRL" \
+helm install ray-cluster "<Path_to_folder>/nemo-rl-on-gke/nemoRL" \
   --set values.additionalWorkerGroups.worker-grp-0.replicas=$REPLICA_COUNT
